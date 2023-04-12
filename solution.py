26b9ad9175df49860a9e45cf633956ba7d93dc11
Evgeny Bessonnitsyn <81041213+Turukmokto@users.noreply.github.com>
2023-04-12 18:14:40 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1112700607  # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+
+def solution(ar1: np.array, ar2: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    cnst = 0.07
+    return mannwhitneyu(ar1, ar2, alternative="less").pvalue < cnst
